73ffc2fbe26120059f28133f11833581ac990810
omdev74 <omdev74@gmail.com>
2021-08-12 05:51:55 +0530

    0.5.11 Css Modules in Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,7 @@
-import styled from "styled-components"
+
 import React from 'react';
-// import './Person.css';
-const StyledDiv = styled.div`
-        
-width: 60%;
-margin: 16px auto;
-border: 1px solid #eee;
-box-shadow: 0 2px 3px #ccc;
-padding: 16px;
-text-align: center;
+import classes from './Person.module.css'
 
-@media(min-width:500px){
-    width: 450px;
-}
-
-`;
 const person = (props)=>{
     // const style = {
     //     "@media(min-width:500px)":{
@@ -23,11 +10,11 @@ const person = (props)=>{
     // }
     return (
         // <div className="Person" style={style}>
-        <StyledDiv>
+        <div className={classes.Person}>
         <p onClick={props.click}>I'm {props.name}! I am {props.age} yers Old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.changed} value={props.name}></input>
-        </StyledDiv>
+        </div>
     );
 };
 export default person;
