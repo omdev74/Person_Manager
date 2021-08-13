5395b13b1f169cf74eea068fe13fc0a4753c8e65
omdev74 <omdev74@gmail.com>
2021-08-14 03:10:51 +0530

    @0.7.8 Component Update Lifecycle (for props Changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,8 +2,32 @@ import React ,{Component}from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component{
+  // part of create and update life cycle as here is no state so not recommended 
+  // though it will not give an error
+
+  // static getDerivedStateFromProps(props,state){
+  //   console.log('[Persons.js] getDerivedStateFromProps')
+  // }
+
+  shouldComponentUpdate(nextProps,nextState){
+    console.log('[Persons.js] shouldComponentUpdate')
+    return true;
+    //generally not hard coded it is conditional
+  }
+  getSnapshotBeforeUpdate(prevProps,prepState){
+    console.log('[Persons.js] getSnapshotBeforeUpdate')
+    return {message: "SnapShot"};//snapshot value
+    //this is datapackage used to store various things like scrolling position 
+    // and passed on to omponentDidUpdate
+  }
+
+  componentDidUpdate(prevProps,prevState,snapshot){
+    console.log('[Persons.js] componentDidUpdate')
+    console.log(snapshot)
+  }
   render(){
     console.log('[Persons.js] renderring...')
+    
   
   return this.props.persons.map((person,index)=>{
             return (
