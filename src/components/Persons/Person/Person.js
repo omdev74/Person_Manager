7d56ca216f5c33604c84a2147952a1e89b5a58b5
omdev74 <omdev74@gmail.com>
2021-08-15 08:16:17 +0530

    @0.7.22 HOC implemented (Auxiliary & WithClass

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-
+import Aux from '../../../hoc/Auxiliary';
 import React ,{Component}from 'react';
 import classes from './Person.module.css'
 
@@ -6,15 +6,16 @@ class Person extends Component{
     render(){
         console.log('[Person.js] renderring...')
         return (
-       
-            <div className={classes.Person}>
+            <Aux classes={classes.Person}>
+            {/* <div className={classes.Person}> */}
             <p onClick={this.props.click}>I'm {this.props.name}! I am {this.props.age} yers Old</p>
             <p>{this.props.children}</p>
             <input 
             type="text" 
             onChange={this.props.changed} 
             value={this.props.name}></input>
-            </div>
+            {/* // </div> */}
+            </Aux>
         );
     }
 }
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,4 @@
+
+const aux = (props)=>(<div className={props.classes}>{props.children}</div>);
+
+export default aux;
